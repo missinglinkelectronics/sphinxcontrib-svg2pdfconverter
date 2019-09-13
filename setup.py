8cb97791a683caceee1bc9f32a7fda31bc0fe7a1
Stefan Wiehler <stefan.wiehler@missinglinkelectronics.com>
2019-09-13 15:40:17 +0200

    Set development status to production
    Signed-off-by: Stefan Wiehler <stefan.wiehler@missinglinkelectronics.com>

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     long_description_content_type='text/x-rst',
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
